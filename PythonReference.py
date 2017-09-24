f7f9f9fbe820bd44caab10caf1a7f0357e5c5ba3
abdelaziz21 <31153357+abdelaziz21@users.noreply.github.com>
2017-09-24 15:47:11 +0200

    Add files via upload

---
+++ PythonReference.py
@@ -0,0 +1,705 @@
+'''
+Created on Sep 23, 2017
+
+@author: ******
+'''
+
+#variables and types
+a, b = 3, 4
+print(a,b)
+
+one = 1
+two = 2.0
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
+
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+
+if mystring == "hello":
+    print("String: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+    
+#Lists
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0]) # prints 1
+print(mylist[1]) # prints 2
+print(mylist[2]) # prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
+    
+mylist = [1,2,3]
+print(mylist)
+
+numbers=[]
+strings=[]
+
+numbers.append(1)
+numbers.append(2)
+
+#basic operators
+number = 9 - 1 + 2 * 3 / 4.0
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared = 7 ** 2
+cubed = 2 ** 3
+
+helloworld = "hello" + " " + "world"
+print(helloworld)
+
+lotsofhellos = "hello" * 3
+print(lotsofhellos)
+
+even_numbers = [2,4,6,8]
+odd_numbers = [1,3,5,7]
+all_numbers = odd_numbers + even_numbers
+print(all_numbers)
+
+print([1,2,3] * 3)
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
+    
+#string formatting
+# This prints out "Hello, John!"
+name = "John"
+print("Hello, %s!" % name)
+
+# This prints out "John is 23 years old."
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age))
+
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
+
+data = ("John", "Doe", 53.44)
+format_string = "Hello %s %s. Your current balance is $%s."
+
+print(format_string % data)
+
+#Basic String Operations
+astring = "Hello world!"
+print("single quotes are ' '")
+
+print(len(astring))
+
+
+astring = "Hello world!"
+print(astring.index("o"))
+
+astring = "Hello world!"
+print(astring.count("l"))
+
+astring = "Hello world!"
+print(astring[3:7])
+
+astring = "Hello world!"
+print(astring[3:7])
+print(astring[3:7:1])
+
+astring = "Hello world!"
+print(astring[::-1])
+
+astring = "Hello world!"
+print(astring.upper())
+print(astring.lower())
+
+astring = "Hello world!"
+print(astring.startswith("Hello"))
+print(astring.endswith("asdfasdfasdf"))
+
+s = "Strings are awesome!"
+# Length should be 20
+print("Length of s = %d" % len(s))
+
+# First occurrence of "a" should be at index 8
+print("The first occurrence of the letter a = %d" % s.index("a"))
+
+# Number of a's should be 2
+print("a occurs %d times" % s.count("a"))
+
+# Slicing the string into bits
+print("The first five characters are '%s'" % s[:5]) # Start to 5
+print("The next five characters are '%s'" % s[5:10]) # 5 to 10
+print("The thirteenth character is '%s'" % s[12]) # Just number 12
+print("The characters with odd index are '%s'" %s[1::2]) #(0-based indexing)
+print("The last five characters are '%s'" % s[-5:]) # 5th-from-last to end
+
+# Split the string into three separate strings,
+# each containing only a word
+print("Split the words of the string: %s" % s.split(" "))
+
+#Conditions
+x = 2
+print(x == 2) # prints out True
+print(x == 3) # prints out False
+print(x < 3) # prints out True
+
+name = "John"
+age = 23
+if name == "John" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
+    
+name = "John"
+if name in ["John", "Rick"]:
+    print("Your name is either John or Rick.")
+
+x = 2
+if x == 2:
+    print("x equals two!")
+else:
+    print("x does not equal to two.")
+    
+x = 5
+if x == 5:
+    print("x equals five!")
+else:
+    print("x does not equal to two.")
+    
+x = [1,2,3]
+y = [1,2,3]
+print(x == y) # Prints out True
+print(x is y) # Prints out False
+
+print(not False) # Prints out True
+print((not False) == (False)) # Prints out False
+
+number = 16
+second_number = 0
+first_array = [1,2,3]
+second_array = [1,2]
+
+if number > 15:
+    print("1")
+
+if first_array:
+    print("2")
+
+if len(second_array) == 2:
+    print("3")
+
+if len(first_array) + len(second_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
+    
+#Loops
+primes = [2, 3, 5, 7]
+for prime in primes:
+    print(prime)
+    
+# Prints out the numbers 0,1,2,3,4
+for x in range(5):
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+    print(x)
+    
+# Prints out 0,1,2,3,4
+count = 0
+while count < 5:
+    print(count)
+    count += 1  # This is the same as count = count + 1
+
+# Prints out 0,1,2,3,4
+
+count = 0
+while True:
+    print(count)
+    count += 1
+    if count >= 5:
+        break
+
+# Prints out only odd numbers - 1,3,5,7,9
+for x in range(10):
+    # Check if x is even
+    if x % 2 == 0:
+        continue
+    print(x)
+    
+#Prints out 0,1,2,3,4 and then it prints "count value reached 5"
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+
+# Prints out 1,2,3,4
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i)
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
+    
+#functions
+#define 3 functions
+def my_function():
+    print("Hello From My Function!")
+    
+def my_function_with_args(username, greeting):
+    print("Hello, %s , From My Function!, I wish you %s"%(username, greeting))
+    
+def sum_two_numbers(a, b):
+    return a + b
+
+# print(a simple greeting)
+my_function()
+
+#prints - "Hello, John Doe, From My Function!, I wish you a great year!"
+my_function_with_args("John Doe", "a great year!")
+
+# after this line x will hold the value 3!
+x = sum_two_numbers(1,2)
+
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return "More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return "%s is a benefit of functions!" % benefit
+
+
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
+
+#Classes and Objects
+class MyClass:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+        
+#assigns a class to an object
+class MyClass1:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass1()
+#accessing and printing out object variable
+class MyClass2:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass2()
+
+myobjectx.variable
+
+print(myobjectx.variable)
+
+#create multiple different objects that are of the same class
+#but each object contains independent copies of the variables define in the class
+class MyClass3:
+    variable = "blah"
+
+    def function(self):
+        print("This is a message inside the class.")
+
+myobjectx = MyClass3()
+myobjecty = MyClass3()
+
+myobjecty.variable = "yackity"
+
+# Then pring out both values
+print(myobjectx.variable)
+print(myobjecty.variable)
+
+# define the Vehicle class
+class Vehicle:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+car1 = Vehicle()
+car1.name = "Fer"
+car1.color = "red"
+car1.kind = "convertible"
+car1.value = 60000.00
+
+car2 = Vehicle()
+car2.name = "Jump"
+car2.color = "blue"
+car2.kind = "van"
+car2.value = 10000.00
+
+print(car1.description())
+print(car2.description())
+#Dictionaries
+phonebook = {}
+phonebook["John"] = 938477566
+phonebook["Jack"] = 938377264
+phonebook["Jill"] = 947662781
+print(phonebook)
+
+phonebook = {
+    "John" : 938477566,
+    "Jack" : 938377264,
+    "Jill" : 947662781
+}
+print(phonebook)
+
+#iterating over dictionaries
+phonebook = {"John" : 938477566,"Jack" : 938377264,"Jill" : 947662781}
+for name, number in phonebook.items():
+    print("Phone number of %s is %d" % (name, number))
+    
+#removing a value
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+del phonebook["John"]
+print(phonebook)
+
+#or remove a value by
+phonebook = {
+   "John" : 938477566,
+   "Jack" : 938377264,
+   "Jill" : 947662781
+}
+phonebook.pop("John")
+print(phonebook)
+
+# testing code
+if "Jack" in phonebook:
+    print("Jake is listed in the phonebook.")
+if "John" not in phonebook:
+    print("John is not listed in the phonebook.")
+    
+#Generators
+import random
+
+def lottery():
+    # returns 6 numbers between 1 and 40
+    for i in range(6):
+        yield random.randint(1, 40)
+
+    # returns a 7th number between 1 and 15
+    yield random.randint(1,15)
+
+for random_number in lottery():
+       print("And the next number is... %d!" %(random_number))
+       
+a = 1
+b = 2
+a, b = b, a
+print(a,b)
+
+# fill in this function
+def fib():
+    a, b = 1, 1
+    while 1:
+        yield a
+        a, b = b, a + b
+
+# testing code
+import types
+if type(fib()) == types.GeneratorType:
+    print("Good, The fib function is a generator.")
+
+    counter = 0
+    for n in fib():
+        print(n)
+        counter += 1
+        if counter == 10:
+            break
+
+#List Comprehensions
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = []
+for word in words:
+      if word != "the":
+          word_lengths.append(len(word))
+print(words)
+
+#using a list comprehension for above
+sentence = "the quick brown fox jumps over the lazy dog"
+words = sentence.split()
+word_lengths = [len(word) for word in words if word != "the"]
+print(words)
+
+numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
+newlist = [int(x) for x in numbers if x > 0]
+print(newlist)
+
+#multiple function arguments
+def foo(first, second, third, *therest):
+    print("First: %s" %(first))
+    print("Second: %s" %(second))
+    print("Third: %s" %(third))
+    print("And all the rest... %s" %(list(therest)))
+
+foo(1,2,3,4,5)
+
+def bar(first, second, third, **options):
+    if options.get("action") == "sum":
+        print("The sum is: %d" %(first + second + third))
+
+    if options.get("number") == "first":
+        return first
+
+result = bar(1, 2, 3, action = "sum", number = "first")
+print("Result: %d" %(result))
+
+# edit the functions prototype and implementation
+def foo(a, b, c, *args):
+    return len(args)
+
+def bar(a, b, c, **kwargs):
+    return kwargs["magicnumber"] == 7
+
+
+# test code
+if foo(1,2,3,4) == 1:
+    print("Good.")
+if foo(1,2,3,4,5) == 2:
+    print("Better.")
+if bar(1,2,3,magicnumber = 6) == False:
+    print("Great.")
+if bar(1,2,3,magicnumber = 7) == True:
+    print("Awesome!")
+    
+#regular expressions
+# Exercise: make a regular expression that will match an email
+import re
+def test_email(your_pattern):
+    pattern = re.compile(your_pattern)
+    emails = ["john@example.com", "python-list@python.org", "wha.t.`1an?ug{}ly@email.com"]
+    for email in emails:
+        if not re.match(pattern, email):
+            print("You failed to match %s" % (email))
+        elif not your_pattern:
+            print("Forgot to enter a pattern!")
+        else:
+            print("Pass")
+# Your pattern here!
+pattern = r"\"?([-a-zA-Z0-9.`?{}]+@\w+\.\w+)\"?"
+test_email(pattern)
+
+#exception handling
+def do_stuff_with_number(n):
+        print(n)
+
+the_list = (1, 2, 3, 4, 5)
+
+for i in range(20):
+    try:
+        do_stuff_with_number(the_list[i])
+    except IndexError: # Raised when accessing a non-existing index of a list
+        do_stuff_with_number(0)
+
+ #Sets       
+print(set("my name is Eric and Eric is my name".split()))
+
+a = set(["Jake", "John", "Eric"])
+print(a)
+b = set(["John", "Jill"])
+print(b)
+
+a = set(["Jake", "John", "Eric"])
+b = set(["John", "Jill"])
+
+print(a.intersection(b))
+
+a = set(["Jake", "John", "Eric"])
+b = set(["John", "Jill"])
+
+print(a.symmetric_difference(b))
+print(b.symmetric_difference(a))
+
+a = set(["Jake", "John", "Eric"])
+b = set(["John", "Jill"])
+
+print(a.difference(b))
+print(b.difference(a))
+
+#Serialization
+import json
+json_string = json.dumps([1, 2, 3, "a", "b", "c"])
+print(json_string)
+
+import pickle
+pickled_string = pickle.dumps([1, 2, 3, "a", "b", "c"])
+print(pickle.loads(pickled_string))
+
+import json
+
+# fix this function, so it adds the given name
+# and salary pair to salaries_json, and return it
+def add_employee(salaries_json, name, salary):
+    salaries = json.loads(salaries_json)
+    salaries[name] = salary
+
+    return json.dumps(salaries)
+
+# test code
+salaries = '{"Alfred" : 300, "Jane" : 400 }'
+new_salaries = add_employee(salaries, "Me", 800)
+decoded_salaries = json.loads(new_salaries)
+print(decoded_salaries["Alfred"])
+print(decoded_salaries["Jane"])
+print(decoded_salaries["Me"])
+print(b.intersection(a))
+
+#Partial Functions
+from functools import partial
+
+def multiply(x,y):
+        return x * y
+
+# create a new function that multiplies by 2
+dbl = partial(multiply,2)
+print(dbl(4))
+
+from functools import partial
+def func(u,v,w,x):
+    return u*4 + v*3 + w*2 + x
+
+p = partial(func,5,6,7)
+print(p(8))
+
+#Code Introspection
+# Use the help function to see what each function does.
+# Delete this when you are done.
+help(dir)
+help(hasattr)
+help(id)
+
+# Define the Vehicle class.
+class Vehicle1:
+    name = ""
+    kind = "car"
+    color = ""
+    value = 100.00
+    def description(self):
+        desc_str = "%s is a %s %s worth $%.2f." % (self.name, self.color, self.kind, self.value)
+        return desc_str
+
+# Print a list of all attributes of the Vehicle class.
+print(dir(Vehicle1))
+
+#Closures
+def transmit_to_space(message):
+    "This is the enclosing function"
+    def data_transmitter():
+        "The nested function"
+        print(message)
+
+    data_transmitter()
+
+print(transmit_to_space("Test message"))
+
+def multiplier_of(n):
+    def multiplier(number):
+        return number*n
+    return multiplier
+
+multiplywith5 = multiplier_of(5)
+print(multiplywith5(9))
+
+#Decorators
+#A decorator is just another function which takes a functions and returns one.
+def repeater(old_function):
+    def new_function(*args, **kwds): #See learnpython.org/page/Multiple%20Function%20Arguments for how *args and **kwds works
+        old_function(*args, **kwds) #we run the old function
+        old_function(*args, **kwds) #we do it twice
+        return new_function #we have to return the new_function, or it wouldn't reassign it to the value
+
+def type_check(correct_type):
+    def check(old_function):
+        def new_function(arg):
+            if (isinstance(arg, correct_type)):
+                return old_function(arg)
+            else:
+                print("Bad Type")
+        return new_function
+    return check
+
+@type_check(int)
+def times2(num):
+    return num*2
+
+print(times2(2))
+times2('Not A Number')
+
+@type_check(str)
+def first_letter(word):
+    return word[0]
+
+print(first_letter('Hello World'))
+first_letter(['Not', 'A', 'String'])
+
+
+
+
+
+
+
+
+
+
+
+
